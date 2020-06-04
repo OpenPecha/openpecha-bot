28d400dcdf2c99c8bfc076a4b86c8ad581ae03df
tenzin <ten13zin@gmail.com>
2020-06-04 18:54:01 +0530

    removed unwanted forms

--- app/bot/forms.py
+++ app/bot/forms.py
@@ -6,15 +6,3 @@ from wtforms.validators import DataRequired, Email, Length
 class PechaSecretKeyForm(FlaskForm):
     secret_key = PasswordField("Pecha Secret Key", validators=[DataRequired()])
     submit = SubmitField("Submit")
-
-
-class InvitationForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    submit = SubmitField("Send Invitation")
-
-
-class PechaIdForm(FlaskForm):
-    pecha_id = StringField(
-        "Pecha Id", validators=[DataRequired(), Length(min=1, max=7)]
-    )
-    submit = SubmitField("Join")
