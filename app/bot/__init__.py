de5fb5d01b2f472d0213c0d1487eb48a9f4dd687
tenzin <ten13zin@gmail.com>
2020-06-10 16:40:09 +0530

    fix sqlite multi-threaded issue

--- app/bot/__init__.py
+++ app/bot/__init__.py
@@ -1,20 +1,23 @@
 import os
 
 from flask import Flask
-from flask_githubapp import GitHubApp
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.pool import StaticPool
 
 from bot.config import Config
-
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 
 # Database setup
-engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+engine = create_engine(
+    app.config["SQLALCHEMY_DATABASE_URI"],
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
 )
