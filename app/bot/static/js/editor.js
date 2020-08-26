439164fb153fd6eccaa07b72587e44baf965e0af
tenzin <ten13zin@gmail.com>
2020-08-27 01:03:10 +0530

    feat: fetch file content

--- app/bot/static/js/editor.js
+++ app/bot/static/js/editor.js
@@ -1,17 +1,31 @@
+function fetchFileContent(download_url) {
+    return fetch(download_url)
+        .then(response => response.text())
+        .then(text => {
+            console.log(text);
+            $(".editor-textarea").text(text)
+        });
+};
+
 
 $(document).ready(function () {
-    // //code here...
-    // var editor = new CodeMirror.fromTextArea($(".editor-textarea")[0], {
-    //     mode: {
-    //         name: "python",
-    //         version: 3,
-    //         singleLineStringErrors: false
-    //     },
-    //     lineNumbers: true,
-    //     theme: "dracula"
-    // });
+    $("body").delegate("#volume-file", "click", function () {
+        var download_url = $(this).children("#file-download-url").val();
+        fetchFileContent(download_url);
+    });
 
-    // var text = editor.getValue()
+    //code here...
+    var editor = new CodeMirror.fromTextArea($(".editor-textarea")[0], {
+        mode: {
+            name: "python",
+            version: 3,
+            singleLineStringErrors: false
+        },
+        lineNumbers: true,
+        theme: "dracula"
+    });
+
+    var text = editor.getValue()
 
 
 });
