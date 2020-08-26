abb8896dd49ad9840460d2e466ee3e03d58f0bc5
tenzin <ten13zin@gmail.com>
2020-08-26 21:50:25 +0530

    feat(front-end): fetch repo's files and display them

---
+++ app/bot/static/js/editor.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function () {
+    // //code here...
+    // var editor = new CodeMirror.fromTextArea($(".editor-textarea")[0], {
+    //     mode: {
+    //         name: "python",
+    //         version: 3,
+    //         singleLineStringErrors: false
+    //     },
+    //     lineNumbers: true,
+    //     theme: "dracula"
+    // });
+
+    // var text = editor.getValue()
+
+
+});
