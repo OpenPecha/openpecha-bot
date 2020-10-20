ff59ef7f1290caf4216d24bd5a86df25d110c3f4
tenzin <ten13zin@gmail.com>
2020-10-20 10:40:34 +0530

    basic handle create pecha form

--- editor/pecha/views.py
+++ editor/pecha/views.py
@@ -8,9 +8,27 @@ from .models import Pecha
 blueprint = Blueprint("pecha", __name__, url_prefix="/pecha", static_folder="../static")
 
 
-# @blueprint.route("/create", method=["POST"])
-# def create_pecha():
-#     pass
+@blueprint.route("/create", methods=["POST"])
+def create_pecha():
+    title = request.form.get("title")
+    sub_title = request.form.get("sub-title")
+    author = request.form.get("author")
+    sku = request.form.get("sku")
+    front_cover_image = request.files.get("front-cover-image")
+    publication_data_image = request.files.get("publication-data-image")
+    content_images = request.files.getlist("content-images")
+    content_text = request.form.get("content-text")
+    print(
+        title,
+        sub_title,
+        author,
+        sku,
+        front_cover_image,
+        publication_data_image,
+        content_images,
+        content_text,
+    )
+    return "Create new pecha"
 
 
 @blueprint.route("/secret", methods=["POST"])
